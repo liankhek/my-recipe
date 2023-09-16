75d6eb3b451cde9c047d121a9ff7d03b74c5f322
liankhek <liankhek@gmail.com>
2023-09-15 23:45:00 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-alert('Hello World');
-let favoriteFood = 'noodle';
-document.write(favoriteFood);
+let recipeList = [];
+
+let recipe1 = {
+    name: 'Fried rice',
+    ingredients: ['Cooked rice', '1egg', 'garlic', 'blackpepper', 'salt', 'oil'],
+    instructions: 'fry garlic and egg, add rice and salt after that and finish up with blackpepper'
+};
+
+let recipe2 = {
+    name: 'Vegetable',
+    ingredients: ['any Vege', 'garlic','onion', 'salt', 'oyster sauce', 'oil'],
+    instructions: 'stir-fry garlic and onion , add oyster sauce and serve'
+};
+
+let recipe3 = {
+    name: 'Egg-Drop soup',
+    ingredients: ['1egg', 'water', 'blackpepper', 'salt'],
+    instructions: 'boil water, add egg and salt, finish up with blackpepper'
+};
+
+recipeList.push(recipe1,recipe2,recipe3);
