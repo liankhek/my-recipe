1be2a1f45bbfb5d980b2347beb14d86aaf871d5e
liankhek <liankhek@gmail.com>
2023-09-30 19:37:58 -0500

    update
    add IIFE
    update CSS & HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,6 @@
-let recipeList = [];
 
-let recipe1 = {
-    name: 'Fried rice',
-    ingredients: ['Cooked rice', '1egg', 'garlic', 'blackpepper', 'salt', 'oil'],
-    instructions: 'fry garlic and egg, add rice and salt after that and finish up with blackpepper'
-};
 
-let recipe2 = {
-    name: 'Vegetable',
-    ingredients: ['any Vege', 'garlic','onion', 'salt', 'oyster sauce', 'oil'],
-    instructions: 'stir-fry garlic and onion , add oyster sauce and serve'
-};
 
-let recipe3 = {
-    name: 'Egg-Drop soup',
-    ingredients: ['1egg', 'water', 'blackpepper', 'salt'],
-    instructions: 'boil water, add egg and salt, finish up with blackpepper'
-};
-
-recipeList.push(recipe1,recipe2,recipe3);
 
 // Loop through recipeList and display recipes with ingredients and instructions
 /*
@@ -42,10 +24,44 @@ for (let i = 0; i < recipeList.length; i++) {
     }
 }
 */
-
-recipeList.forEach(function (recipe) {
+let recipeRepository = (function () {
+    let recipeList = [];
+  
+    let recipe1 = {
+      name: 'Fried rice',
+      ingredients: ['Cooked rice', '1 egg', 'garlic', 'black pepper', 'salt', 'oil'],
+      instructions: 'Fry garlic and egg, add rice and salt after that, and finish up with black pepper'
+    };
+  
+    let recipe2 = {
+      name: 'Vegetable',
+      ingredients: ['any Vege', 'garlic', 'onion', 'salt', 'oyster sauce', 'oil'],
+      instructions: 'Stir-fry garlic and onion, add oyster sauce, and serve'
+    };
+  
+    let recipe3 = {
+      name: 'Egg-Drop soup',
+      ingredients: ['1 egg', 'water', 'black pepper', 'salt'],
+      instructions: 'Boil water, add egg and salt, finish up with black pepper'
+    };
+    
+    recipeList.push(recipe1, recipe2, recipe3);
+  
+    return {
+      add: function (recipe) {
+        recipeList.push(recipe);
+      },
+      getAll: function () {
+        return recipeList;
+      }
+    }
+  })();
+  
+  recipeRepository.getAll().forEach(function (recipe) {
     document.write('Name: ' + recipe.name + '<br>' + 
-    'Ingredients: ' + recipe.ingredients.join(', ') +'<br>' + 
-    'Instructions: ' + recipe.instructions + '<br><br>');
-});
+      'Ingredients: ' + recipe.ingredients.join(', ') + '<br>' + 
+      'Instructions: ' + recipe.instructions + '<br><br>');
+  });
+
+
 
