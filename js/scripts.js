d68fff9138633d81cd882beaf66a79052c99c985
liankhek <liankhek@gmail.com>
2023-10-01 19:35:08 -0500

    Update js
    update IIFE
    add check function
    add filter

--- js/scripts.js
+++ js/scripts.js
@@ -25,37 +25,78 @@ for (let i = 0; i < recipeList.length; i++) {
 }
 */
 let recipeRepository = (function () {
-    let recipeList = [];
+    let recipeList = [
   
-    let recipe1 = {
-      name: 'Fried rice',
-      ingredients: ['Cooked rice', '1 egg', 'garlic', 'black pepper', 'salt', 'oil'],
-      instructions: 'Fry garlic and egg, add rice and salt after that, and finish up with black pepper'
-    };
-  
-    let recipe2 = {
-      name: 'Vegetable',
-      ingredients: ['any Vege', 'garlic', 'onion', 'salt', 'oyster sauce', 'oil'],
-      instructions: 'Stir-fry garlic and onion, add oyster sauce, and serve'
-    };
-  
-    let recipe3 = {
-      name: 'Egg-Drop soup',
-      ingredients: ['1 egg', 'water', 'black pepper', 'salt'],
-      instructions: 'Boil water, add egg and salt, finish up with black pepper'
-    };
+      {
+        name: 'Fried rice',
+        ingredients: ['Cooked rice', '1 egg', 'garlic', 'black pepper', 'salt', 'oil'],
+        instructions: 'Fry garlic and egg, add rice, finish up with black pepper and ready to be served.'
+      },
     
-    recipeList.push(recipe1, recipe2, recipe3);
+      {
+        name: 'Vegetable',
+        ingredients: ['any Vege', 'garlic', 'onion', 'salt', 'oyster sauce', 'oil'],
+        instructions: 'Stir-fry garlic and onion, add oyster sauce, and ready to be served.'
+      },
+    
+      {
+        name: 'Egg-Drop soup',
+        ingredients: ['1 egg', 'water', 'black pepper', 'salt'],
+        instructions: 'Boil water, add egg and salt, finish up with black pepper and ready to be served.'
+      }
+    ];
   
     return {
       add: function (recipe) {
         recipeList.push(recipe);
       },
+
+      addn: function (recipe){
+        if (typeof recipe === 'object'){
+          let expectedKeys = ['name', 'ingredients', 'instructions'];
+          let keyMatch = expectedKeys.every(function(key){
+            return Object.keys(recipe).includes(key);
+          });
+          if (keyMatch) {
+            recipeList.push(recipe);
+          }
+          else {
+            console.error ('Invalid recipe! Please provide a recipe(object) with the keys: names, ingredients, instructions.');
+          }
+        }
+        else {
+          console.error('Invalid input! Please provide a valid recipe object.');
+        }
+      },
+
       getAll: function () {
         return recipeList;
+      },
+
+      findByName: function () {
+        return recipeList.find(function (recipe){
+          return recipe.name === name;
+        });
+      },
+      
+      filterByProperty: function (property, value) {
+        return recipeList.filter(function (recipe) {
+          return recipe[property] === value;
+        });
       }
     }
   })();
+
+  let newRecipe = {
+    name: 'Alfredo spaghetti',
+    ingredients: ['Spaghetti', 'chicken', 'cheese', 'Alfredo'],
+    instructions: 'Cook spaghetti, grill chicken, cheese, add alfredo and ready to be served.'
+  }
+
+  recipeRepository.addn(newRecipe);
+
+  let filteredRecipes = recipeRepository.filterByProperty ('name', 'Fried rice');
+  console.log ('Filtered Recipes:', filteredRecipes);
   
   recipeRepository.getAll().forEach(function (recipe) {
     document.write('Name: ' + recipe.name + '<br>' + 
